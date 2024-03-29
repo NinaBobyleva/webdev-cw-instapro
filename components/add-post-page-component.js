69e1b283bf867cb9c8f8d0f49d33bf1054c3364b
Nina Bobyleva <2847171@mail.ru>
2024-03-29 13:24:27 +0300

    add post page

--- components/add-post-page-component.js
+++ components/add-post-page-component.js
@@ -1,13 +1,28 @@
+import { renderHeaderComponent } from "./header-component.js";
+
 export function renderAddPostPageComponent({ appEl, onAddPostClick }) {
   const render = () => {
     // TODO: Реализовать страницу добавления поста
     const appHtml = `
     <div class="page-container">
       <div class="header-container"></div>
-      Cтраница добавления поста
-      <button class="button" id="add-button">Добавить</button>
-    </div>
-  `;
+      <div class="page-container">
+          <div class="header-container"></div>
+          <div class="form">
+              <h3 class="form-title">
+                Добавить пост
+                </h3>
+              <div class="form-inputs">
+              <p>Опишите фотографию:</p>
+                  <textarea type="text" id="login-input" class="textarea"/></textarea>
+                  <div class="form-error"></div>
+                  <button class="button" id="login-button">Добавить</button>
+              </div>
+              <div class="form-footer">
+              </div>
+          </div>
+      </div>    
+`;
 
     appEl.innerHTML = appHtml;
 
@@ -18,6 +33,10 @@ export function renderAddPostPageComponent({ appEl, onAddPostClick }) {
       });
     });
   };
+
+  // renderHeaderComponent({
+  //   element: document.querySelector(".header-container"),
+  // });
 
   render();
 }
