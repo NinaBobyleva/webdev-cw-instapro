39b24c7b4d14909ce5d6fbd9635db46d8433b00b
Nina Bobyleva <2847171@mail.ru>
2024-04-06 19:49:30 +0300

    correction

--- components/posts-page-component.js
+++ components/posts-page-component.js
@@ -14,7 +14,7 @@ export function renderPostsPageComponent({ appEl }) {
    * TODO: чтобы отформатировать дату создания поста в виде "19 минут назад"
    * можно использовать https://date-fns.org/v2.29.3/docs/formatDistanceToNow
    */
-  const appHtml = posts.map((post, index) => {
+  let appHtml = posts.map((post, index) => {
     return `<div class="page-container">
                 <div class="header-container"></div>
                 <ul class="posts">
@@ -45,7 +45,17 @@ export function renderPostsPageComponent({ appEl }) {
                 </ul>
               </div>`}).join('');
 
-  appEl.innerHTML = appHtml;
+  if (posts.length === 0) {
+    appHtml = posts.map(() => {
+      return appEl.innerHTML = 
+      `<div class="page-container">
+      <div class="header-container"></div>
+      </div>`}).join('');
+    } else {
+      appEl.innerHTML = appHtml;
+    }
+
+  
 
   
   for (const likeButtonElement of document.querySelectorAll('.like-button')) {
@@ -81,8 +91,47 @@ export function renderPostsPageComponent({ appEl }) {
       goToPage(USER_POSTS_PAGE, {
         userId: userEl.dataset.userId,
       });
+      console.log(userEl.dataset.userId);
     });
   }
 
 
 }
+
+
+
+{/* <li class="post">
+            <div class="post-header" data-user-id=${posts.id}>
+                <img src="https://storage.yandexcloud.net/skypro-webdev-homework-bucket/1680601903167-%25C3%2590%25C2%25A1%25C3%2590%25C2%25BD%25C3%2590%25C2%25B8%25C3%2590%25C2%25BC%25C3%2590%25C2%25BE%25C3%2590%25C2%25BA%2520%25C3%2591%25C2%258D%25C3%2590%25C2%25BA%25C3%2591%25C2%2580%25C3%2590%25C2%25B0%25C3%2590%25C2%25BD%25C3%2590%25C2%25B0%25202023-03-31%2520%25C3%2590%25C2%25B2%252012.45.42.png" class="post-header__user-image">
+                <p class="post-header__user-name">Нет данных для отопрбажения</p>
+            </div>
+            </li> */}
+
+
+
+{/* <ul class="posts">
+        <li class="post">
+          <div class="post-header" data-user-id="642d00329b190443860c2f31">
+              <img src="https://www.imgonline.com.ua/examples/bee-on-daisy.jpg" class="post-header__user-image">
+              <p class="post-header__user-name">Иван Иваныч</p>
+          </div>
+          <div class="post-image-container">
+            <img class="post-image" src="https://www.imgonline.com.ua/examples/bee-on-daisy.jpg">
+          </div>
+          <div class="post-likes">
+            <button data-post-id="642d00579b190443860c2f32" class="like-button">
+              <img src="./assets/images/like-active.svg">
+            </button>
+            <p class="post-likes-text">
+              Нравится: <strong>2</strong>
+            </p>
+          </div>
+          <p class="post-text">
+            <span class="user-name">Иван Иваныч</span>
+            Ромашка, ромашка...
+          </p>
+          <p class="post-date">
+            19 минут назад
+          </p>
+        </li>
+          </ul> */}
